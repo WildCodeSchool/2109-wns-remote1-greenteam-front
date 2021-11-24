602587f34a2c622e88c96ff39cef72ebb4ee50f8
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-11-24 17:27:56 +0100

    styled components installed and side bar component content initialized

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const SideBarContainer = styled.div`
+  width: 150px;
+  background-color: #518F97;
+  height: 100vh;
+`;
+
+const navLinks = [
+  {pageTitle: "Home", link: "/home"},
+  {pageTitle: "Projects", link: "/projects"},
+  {pageTitle: "Admin", link: "/admin"}
+]
 
 export default function SideBar(): JSX.Element {
   return (
-    <div>
-      Side Bar
-    </div>
+    <SideBarContainer>
+      side bar
+    </SideBarContainer>
   )
 }
 
