13148a9c3769fb803ac8119d0357a447d9bf62d6
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-11-25 16:58:31 +0100

    side bar and small side bar created and styled

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import LeftArrow from '../assets/icons/left-arrow.svg';
+import RightArrow from '../assets/icons/right-arrow.svg';
+import HiveLogo from '../assets/icons/hive.svg';
+import CollabeeLogo from '../assets/icons/app-logo.svg';
+import HomeLogo from '../assets/icons/home-logo.svg';
+import ProjectsLogo from '../assets/icons/projects-logo.svg';
+import AdminLogo from '../assets/icons/admin-logo.svg';
 
-const SideBarContainer = styled.div`
-  width: 150px;
-  background-color: #518F97;
-  height: 100vh;
-`;
+
+const LogoContainer = styled.p`
+display: flex;
+justify-content: center;
+margin-bottom: 70px;
+height: 41px;
+`
 
 const navLinks = [
-  {pageTitle: "Home", link: "/home"},
-  {pageTitle: "Projects", link: "/projects"},
-  {pageTitle: "Admin", link: "/admin"}
+  {pageTitle: "Home", link: "/home", icon: HomeLogo},
+  {pageTitle: "Projects", link: "/projects", icon: ProjectsLogo},
+  {pageTitle: "Admin", link: "/admin", icon: AdminLogo}
 ]
 
 export default function SideBar(): JSX.Element {
+  
+  const [showSmallSideBar, setShowSmallSideBar] = useState(false);
+
+  const LeftArrowContainer = styled.div`
+  display: flex;
+  justify-content: ${showSmallSideBar ? 'center' : 'end'};
+  padding: ${showSmallSideBar ? '10px 0 0 0' : '10px 10px 0 0'};
+  cursor: pointer;
+  margin-bottom: 24px;
+  `;
+
+  const SideBarContainer = styled.div`
+  width: ${showSmallSideBar ? '50px' : '160px'};
+  background-color: #518F97;
+  height: 100vh;
+  `;
+
+  const PageTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  height: 33px;
+  margin-bottom: 25px;
+  `;
+  
+  const toggleShowSmallSideBar = () => {
+    setShowSmallSideBar(!showSmallSideBar);
+    
+  }
+
   return (
     <SideBarContainer>
-      {navLinks.map(({ pageTitle }) => (
-        <div>
-          <p>{pageTitle}</p>
+      <LeftArrowContainer onClick={toggleShowSmallSideBar}>
+        <img src={showSmallSideBar ? RightArrow : LeftArrow} alt="left-arrow"/>
+      </LeftArrowContainer>
+      <LogoContainer>
+        <img src={showSmallSideBar ? HiveLogo : CollabeeLogo} alt="collabee-logo"/>
+      </LogoContainer>
+     
+      {navLinks.map(({ pageTitle, link, icon }) => (
+        <div key={link}>
+        <PageTitle>{showSmallSideBar ? <img src={icon} alt="bar-icon" /> : pageTitle}</PageTitle>
         </div>
       ))}
     </SideBarContainer>
