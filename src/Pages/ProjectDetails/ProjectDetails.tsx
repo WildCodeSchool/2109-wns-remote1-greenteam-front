474d5b38b59e5c3b8964bc054264013442691b95
aminergb <darkyalb144@gmail.com>
2022-01-21 14:11:55 +0100

    creation de projectdetails

--- src/Pages/ProjectDetails/ProjectDetails.tsx
+++ src/Pages/ProjectDetails/ProjectDetails.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import DetailComponent from './DetailComponent';
+import projectdetailspic from '../../assets/pictures/projectdetails/detailpic.svg'
 
 export default function ProjectDetails(): JSX.Element {
   return (
     <div className='flex flex-col w-screen h-screen justify-between'>
       <div className='flex justify-center align-center items-center h-1/6' >
-        <p className='min-w-fitcontent max-w-full'>project title</p>
+        <p className='min-w-fitcontent max-w-full text-4xl'>project title</p>
       </div>
-      <div className='flex flex-row  w-full h-4/5 justify-around items-center'>
-        <div className='flex-1 h-4/5'><img src="https://www.google.com/chrome/static/images/intl/fr_FR/homepage/laptop_desktop.png" alt="" /></div>
-        <div className='flex-1 h-4/5 flex-col flex'><DetailComponent /></div>
+      <div className='flex flex-row  w-full h-5/6 justify-around items-center'>
+        <div className='flex flex-row w-1/4 h-4/5'><img src={projectdetailspic} alt="" /></div>
+        <div className='flex justify-center h-full w-8/12 flex-row'><DetailComponent /></div>
       </div>
     </div>);
 }
