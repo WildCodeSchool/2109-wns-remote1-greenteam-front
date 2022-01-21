a958252eff17bb78eccda1d07d7e1bf9af033b25
aminergb <darkyalb144@gmail.com>
2022-01-21 11:00:04 +0100

    init_homepage

--- src/Pages/HomePage/ContentComponent.tsx
+++ src/Pages/HomePage/ContentComponent.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function ContentComponent({ imgsrc, text }: Partial<any>): JSX.Element {
 
     return (<>
-        <div className='h-4/6 flex flex-col justify-between'>
-            <img className='w-homeimgwidth h-homeimgheight ' alt="" src={imgsrc} />
-            <p className='text-textelementsize'>{text}</p>
+        <div className='h-full w-1/3 flex flex-col   items-center flex-nowrap'>
+            <img alt="home-img" src={imgsrc} />
+            <p className='text-lg text-center mt-6'>{text}</p>
         </div>
     </>)
 }
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import ContentComponent from './ContentComponent';
 import { HomePageProps } from './HomePageProps';
+import pic1 from '../../assets/pictures/homepage/pic1.svg'
+import pic2 from '../../assets/pictures/homepage/pic2.svg'
 
 
 export default function HomePage({ name }: Partial<HomePageProps>): JSX.Element {
   const contents = [{
-    imgsrc: "https://www.google.com/chrome/static/images/intl/fr_FR/homepage/laptop_desktop.png",
-    text: "hello"
+    imgsrc: pic1,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy "
   },
   {
-    imgsrc: "https://www.google.com/chrome/static/images/intl/fr_FR/homepage/laptop_desktop.png",
-    text: "hey"
+    imgsrc: pic2,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy "
   }]
 
   return (
     <div className='flex flex-col w-screen h-screen align-around justify-between'>
 
       <div className='flex flex-row w-full justify-end flex-initial'>
-        <div className=''>auth</div>
+        <div className=''><span>a</span> <span>b</span></div>
       </div>
-      <div className='font-poppins flex text-titlesize justify-center items-center content-center   h-2/6'>
-        <p>Welcome {name} ! Ready to smash your goals?</p></div>
-      <div className='flex flex-row justify-around flex-wrap  h-3/5 content-start'>
+      <div className='font-poppins text-5xl flex justify-center items-center content-center   h-1/6'>
+        <h1>Welcome {name} ! Ready to smash your goals?</h1></div>
+      <div className='flex flex-row justify-around items-baseline h-4/6 '>
         {contents.map(({ imgsrc, text }) => <ContentComponent imgsrc={imgsrc} text={text} />
         )}
 
